094964f6db8cd9d0ca2e43fb23797d7a35c41c54
Stephen Royle <s.j.royle@warwick.ac.uk>
2019-11-30 15:54:40 +0000

    Python3

--- multiply.py
+++ multiply.py
@@ -2,18 +2,18 @@
 import random, time
 
 #welcome message and get the user's name
-print "Welcome to the multiplication game."
-print "What's your name?"
-name = raw_input("Name: ")
-print "OK,", name, "you will do 5 tests."
-print "Each test will be ten questions and you will have 1 minute for each!"
+print("Welcome to the multiplication game.")
+print("What's your name?")
+name = input("Name: ")
+print("OK,", name, "you will do 5 tests.")
+print("Each test will be ten questions and you will have 1 minute for each!")
 bank = 0
 qright = 0
 
 for game in range(0,5):
     readystr = ""
     while len(readystr) == 0:
-        readystr = raw_input("Ready?")
+        readystr = input("Ready?")
     start = time.time()
 
     for num in range(0,10):
@@ -23,30 +23,30 @@ for game in range(0,5):
         answer = number1 * number2
 
         guess = 0
-        print "What is", number1, "x", number2, "?"
+        print("What is", number1, "x", number2, "?")
 
         while guess != answer:
-            guess = input("Answer: ")
+            guess = int(input("Answer: "))
             if guess != answer:
-                print "  No, try again"
+                print("  No, try again")
 
         curr = time.time()
         timeleft = (60 + bank) - (curr - start)
         if (timeleft > 0):
-            print "  CORRECT!", "You have", int(timeleft), "seconds left."
+            print("  CORRECT!", "You have", int(timeleft), "seconds left.")
             qright = qright + 1
         else:
-            print "  TIME'S UP! Sorry!"
+            print("  TIME'S UP! Sorry!")
             break
 
     if (game + 1) * 10 == qright:
         bank = timeleft
-        print "That was test", game + 1
+        print("That was test", game + 1)
         if (game + 1 < 5):
-            print name, "YOU DID IT! Now onto test", game + 2
+            print(name, "YOU DID IT! Now onto test", game + 2)
         else:
-            print name, "YOU DID IT! That was amazing! You win!"
+            print(name, "YOU DID IT! That was amazing! You win!")
     else:
-        print "Please try again."
+        print("Please try again.")
         break
-print "G A M E  O V E R"
+print("G A M E  O V E R")
